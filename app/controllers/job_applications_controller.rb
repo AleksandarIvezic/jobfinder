a44bece19dad0ac06158b3f28eac947934497a1b
Aleksandar <a.ivezic@yahoo.com>
2021-08-08 18:32:59 +0200

    Create job_applications controller

---
+++ app/controllers/job_applications_controller.rb
@@ -0,0 +1,66 @@
+class JobApplicationsController < ApplicationController
+  def index 
+    @job_applications = JobApplication.all
+  end
+
+  def show
+    @job_application = JobApplication.find(params[:id])
+  end
+
+  # GET /jobs/new
+  def new
+    @job_application = JobApplication.new
+  end
+
+  # GET /jobs/1/edit
+  def edit
+  end
+
+  # POST /jobs or /jobs.json
+  def create
+    @job_application = JobApplication.new(job_application_params)
+
+    respond_to do |format|
+      if @job_application.save
+        format.html { redirect_to @job_application, notice: "Job application was successfully created." }
+        format.json { render :show, status: :created, location: @job_application }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @job_application.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /jobs/1 or /jobs/1.json
+  def update
+    respond_to do |format|
+      if @job_application.update(job_application_params)
+        format.html { redirect_to @job_application, notice: "Job application was successfully updated." }
+        format.json { render :show, status: :ok, location: @job_application }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @job_application.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /jobs/1 or /jobs/1.json
+  def destroy
+    @job_application.destroy
+    respond_to do |format|
+      format.html { redirect_to job_applications_url, notice: "Job application was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_job
+      @job_application = JobApplication.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def job_application_params
+      params.fetch(:job_application, {:name,:birth_date, :email, :phone, :address, :professional_qualifications, :resume})
+    end
+end
