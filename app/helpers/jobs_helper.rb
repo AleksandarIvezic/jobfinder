10641f8d58142f9d5cdb19e0d92359066085c63b
Aleksandar <a.ivezic@yahoo.com>
2021-08-11 11:37:04 +0200

    Create dateExpire helper

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,7 @@
 module JobsHelper
+  def dateExpire
+    content = ""
+    @job.expiring_date < DateTime.now ? content = "The application deadline has expired" : content = "You can apply until: #{@job.expiring_date.strftime("%d/%m/%Y")}"
+    content;
+  end 
 end
