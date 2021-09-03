3b4fb144eb642cd1facdc58a1804620440c69724
Aleksandar <a.ivezic@yahoo.com>
2021-09-03 14:43:35 +0200

    Add job validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,9 @@
 class Job < ApplicationRecord
   has_many :job_applications, dependent: :destroy
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :creator_id, presence: true
+  validates :category, presence: true
+  validates :expiring_date, presence: true
 end
