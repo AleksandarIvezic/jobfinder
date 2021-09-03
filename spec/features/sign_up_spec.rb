f3a6ad591195b540b5cae1a862e33cfb095d21dd
Aleksandar <a.ivezic@yahoo.com>
2021-09-03 15:46:12 +0200

    Add sign up feature test

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,29 @@
+
+RSpec.describe 'Sign up process', type: :feature do
+  it 'new user sign up successfully' do
+    visit '/users/sign_up'
+    fill_in 'Name', with: 'username'
+    fill_in 'Email', with: 'mail@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+  end
+  it "doesn't sign up if user already exist" do
+    visit '/users/sign_up'
+    fill_in 'Name', with: 'username'
+    fill_in 'Email', with: 'mail@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+    click_link 'Logout'
+    visit '/users/sign_up'
+    fill_in 'Name', with: 'username'
+    fill_in 'Email', with: 'mail@example.com'
+    fill_in 'Password', with:
+     'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+    expect(page).to have_content 'Email has already been taken'
+  end
+end
