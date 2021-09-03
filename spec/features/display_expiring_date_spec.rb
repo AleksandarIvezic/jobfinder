259551b64a2a43458bae0c0804405525c9d15520
Aleksandar <a.ivezic@yahoo.com>
2021-09-03 16:20:18 +0200

    Add tests for displaying Expiring date

---
+++ spec/features/display_expiring_date_spec.rb
@@ -0,0 +1,32 @@
+RSpec.describe "The expiring date", type: :feature do
+  before :each do
+    User.create(name: 'User', email: 'user@example.com', password: 'password', password_confirmation: 'password')
+  end
+
+  it "when deadline is not expired yet, date is displayed" do
+    visit '/users/sign_in'    
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'password'    
+    click_button 'Log in'
+    click_link 'Add New Job'
+    fill_in 'Name', with: 'Job name example'
+    fill_in 'Description', with: 'Job description test field'
+    fill_in 'Category', with: 'Job category'
+    fill_in "Expiring date", with: "01/01/2022"
+    click_button "Create Job"
+    expect(page).to have_content 'You can apply until: 01/01/2022'
+  end
+  it "when deadline is expired yet, date is not displayed" do
+    visit '/users/sign_in'    
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'password'    
+    click_button 'Log in'
+    click_link 'Add New Job'
+    fill_in 'Name', with: 'Job name example'
+    fill_in 'Description', with: 'Job description test field'
+    fill_in 'Category', with: 'Job category'
+    fill_in "Expiring date", with: "01/01/2021"
+    click_button "Create Job"
+    expect(page).to have_content 'The application deadline has expired'
+  end
+end
