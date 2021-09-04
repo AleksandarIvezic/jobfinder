23ba9b2d59769c968f7d92deec8ba9657747569a
Aleksandar <a.ivezic@yahoo.com>
2021-09-04 16:07:16 +0200

    Add mocking file to mailer prevew

--- spec/mailers/previews/confirmation_mailer_preview.rb
+++ spec/mailers/previews/confirmation_mailer_preview.rb
@@ -4,7 +4,10 @@ class ConfirmationMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/confirmation_mailer/applicant
   def applicant
     @job = Job.last
-    @job_application = JobApplication.last
+    
+    @file = File.open("public/apple-touch-icon.png")
+    @job_application = @job.job_applications.create(name: "test",birth_date:'12/12/1989', email:'mail@test.com', phone:'123456789', address:'Test adress', professional_qualifications: 'VSS', resume:@file, job_id:'1')
+   
     @employer = User.find(@job.creator_id);
     ConfirmationMailer.with(job_application: @job_application, job: @job, employer: @employer).applicant
   end
@@ -12,7 +15,10 @@ class ConfirmationMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/confirmation_mailer/employer
   def employer
     @job = Job.last
-    @job_application = JobApplication.last
+    
+    @file = File.open("public/apple-touch-icon.png")
+    @job_application = @job.job_applications.create(name: "test",birth_date:'12/12/1989', email:'mail@test.com', phone:'123456789', address:'Test adress', professional_qualifications: 'VSS', resume:@file, job_id:'1')
+   
     @employer = User.find(@job.creator_id);
     ConfirmationMailer.with(job_application: @job_application,employer: @employer).employer
   end
