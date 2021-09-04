1135e50b006c84d31b17be7f820b2788ab3e5aa3
Aleksandar <a.ivezic@yahoo.com>
2021-09-04 16:33:37 +0200

    Create home page and add some content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :jobs, :job_applications
-  root to: "jobs#index"
+  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
