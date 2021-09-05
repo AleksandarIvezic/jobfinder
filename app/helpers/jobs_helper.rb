ba91d0db34b33539d511c7c1e512aa49ae7574ac
Aleksandar <a.ivezic@yahoo.com>
2021-09-05 20:45:26 +0200

    Create and implement display_employer helper

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,4 +2,8 @@ module JobsHelper
   def dateExpire(job)
     job.expiring_date < DateTime.now ? "The application deadline has expired" : "You can apply until: #{@job.expiring_date.strftime("%d/%m/%Y")}"
   end 
+  def display_employer(job)
+    @user = User.find(job.creator_id)
+    @user.name    
+  end
 end
