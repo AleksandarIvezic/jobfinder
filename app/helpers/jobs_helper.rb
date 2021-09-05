75029181ce8e252834a8d7cd8af68029181d6fe2
Aleksandar <a.ivezic@yahoo.com>
2021-09-05 16:35:37 +0200

    Style job offers page

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,7 +1,5 @@
 module JobsHelper
-  def dateExpire
-    content = ""
-    @job.expiring_date < DateTime.now ? content = "The application deadline has expired" : content = "You can apply until: #{@job.expiring_date.strftime("%d/%m/%Y")}"
-    content;
+  def dateExpire(job)
+    job.expiring_date < DateTime.now ? "The application deadline has expired" : "You can apply until: #{@job.expiring_date.strftime("%d/%m/%Y")}"
   end 
 end
