df7026999dcdffab8be1208280487f1ef7d881fd
Aleksandar <a.ivezic@yahoo.com>
2021-09-05 14:33:04 +0200

    Style nav bar and home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,14 +4,20 @@ module ApplicationHelper
     user_signed_in? ? content =  link_to("Logout", destroy_user_session_path, method: :delete, data: {confirm: 'Confirm Logout'}) : content << link_to("Login", new_user_session_path, class: "nav-link")
     content.html_safe
   end
+  
   def signup
-    content = ''
-    content = link_to("Sign-Up", new_user_registration_path, class: "nav-link") unless user_signed_in?      
-    content.html_safe
+     link_to("Sign-Up", new_user_registration_path, class: "nav-link") unless user_signed_in?      
   end
 
   def my_offers
-    content = ''
-    content = link_to('My offers', user_path, class: nav-link) if user_signed_in?
+    link_to('My offers', user_path, class: nav-link) if user_signed_in? 
+  end
+
+  def display_notice
+    content_tag :p, notice, class: 'notice', id:'notice' if notice
+  end
+
+  def display_alert
+    content_tag :p, alert, class: 'alert', id: 'alert' if alert
   end
 end
