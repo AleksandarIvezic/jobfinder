4fc4751b7c86183c6ccc34d7ebeec1809330c920
Aleksandar <a.ivezic@yahoo.com>
2021-09-05 19:02:58 +0200

    Install action text and style new job page

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,6 @@
 class Job < ApplicationRecord
   has_many :job_applications, dependent: :destroy
+  has_rich_text :description
 
   validates :name, presence: true
   validates :description, presence: true
