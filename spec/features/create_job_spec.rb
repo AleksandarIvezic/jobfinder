c08b32e99eecd1806decff025228dc1b9848b92e
Aleksandar <a.ivezic@yahoo.com>
2021-09-10 20:16:30 +0200

    Fix description tests

--- spec/features/create_job_spec.rb
+++ spec/features/create_job_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe "Create new job offer", type: :feature do
+RSpec.describe "Create new job offer", type: :feature, js: true do
   before :each do
     User.create(name: 'User', email: 'user@example.com', password: 'password', password_confirmation: 'password')
   end
@@ -12,6 +12,7 @@ RSpec.describe "Create new job offer", type: :feature do
     click_button 'Log in'
     click_link 'Create a job offer'
     fill_in 'Job title', with: 'Job name example'
+    find('#job_description').set('Job description test field')
     fill_in 'Category', with: 'Job category'
     fill_in "Expiring date", with: "01/01/2022"
     click_button "Create Job"
