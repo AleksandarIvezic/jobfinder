6d7d7c5daab2f8e42a126a1b73b35664c8abeb4b
Aleksandar <a.ivezic@yahoo.com>
2021-12-19 19:09:21 +0100

    add configuration to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -96,6 +96,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Add defalult url options 
+  config.action_mailer.default_url_options = { host: 'https://aleks-job-finder.herokuapp.com/'}
+  # Add mailer delivery method
+  config.action_mailer.delivery_method = :smtp
+  # Add gmail configuration
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'a.ivezic88@gmail.com',
+    password:             'bdxbkcmzxrqsiafr',
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
